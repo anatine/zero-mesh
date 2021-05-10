6ad767797545fb342adebfbeeb8558bffb9bffe5
Brian McBride <Brian-McBride@users.noreply.github.com>
2021-05-10 16:09:49 -0700

    Base firebase setup

---
+++ apps/gcp/functions/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'gcp-functions',
+  preset: '../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../coverage/apps/gcp/functions',
+};
